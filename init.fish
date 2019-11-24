f2b8c9638dd75b7f6cc4c9bdde7dfe4e17819211
Justin Bennett <zephraph@gmail.com>
2019-11-24 16:59:20 -0500

    Initial plugin setup

---
+++ init.fish
@@ -0,0 +1,26 @@
+# zephish initialization hook
+#
+# You can use the following variables in this file:
+# * $package       package name
+# * $path          package path
+# * $dependencies  package dependencies
+
+set fish_greeting ""
+
+set PATH ./node_modules/.bin $PATH
+
+if type -q code
+    set -x EDITOR "code -w"
+end
+
+if type -q starship
+    starship init fish | source
+end
+
+# Quick edit access for Zephish plugin
+abbr -a EZ code ~/.config/omf/zephish
+
+# General abbreviations
+abbr -a c clear
+abbr -a e exit
+
