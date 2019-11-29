c9d3c105c8e466c929173827a58e0314d2acdd13
Justin Bennett <zephraph@gmail.com>
2019-11-29 02:07:24 -0500

    Add note to init file about execution impacts

--- init.fish
+++ init.fish
@@ -1,5 +1,8 @@
 # zephish initialization hook
 #
+# WARNING: Stuff in this file gets run everytime a new 
+# shell is created. Limit it to the absolute minimum required.
+#
 # You can use the following variables in this file:
 # * $package       package name
 # * $path          package path
