ccfcd2a5b92b8be21411b5baf7a3ca55586dc812
Justin Bennett <zephraph@gmail.com>
2020-09-13 00:23:32 -0400

    Add lua modules and flags needed to build pyenv

--- init.fish
+++ init.fish
@@ -8,7 +8,16 @@
 # * $path          package path
 # * $dependencies  package dependencies
 
-set PATH ./node_modules/.bin ~/go/bin ~/.cargo/bin ~/.bin $PATH
+set PATH ./node_modules/.bin ./lua_modules/bin ~/go/bin ~/.cargo/bin ~/.bin $PATH
+
+switch (uname)
+    case Darwin
+        # Hack to ensure python builds will work
+        # https://github.com/pyenv/pyenv/issues/1219#issuecomment-684140087
+        set PYTHON_CONFIGURE_OPTS "--with-openssl=(brew --prefix openssl)"
+        set CFLAGS "-I(brew --prefix zlib)/include -I(brew --prefix sqlite)/include -I(brew --prefix bzip2)/include"
+        set LDFLAGS "-L(brew --prefix zlib)/lib -L(brew --prefix sqlite)/lib -L(brew --prefix bzip2)/lib"
+end
 
 if type -q starship
     starship init fish | source
