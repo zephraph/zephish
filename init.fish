b69e4b085ad878e62d0fbce016403a242d7d2c1a
Justin Bennett <zephraph@gmail.com>
2021-03-08 16:51:37 -0500

    Add linux brew env setup

--- init.fish
+++ init.fish
@@ -17,6 +17,15 @@ switch (uname)
         set PYTHON_CONFIGURE_OPTS "--with-openssl=(brew --prefix openssl)"
         set CFLAGS "-I(brew --prefix zlib)/include -I(brew --prefix sqlite)/include -I(brew --prefix bzip2)/include"
         set LDFLAGS "-L(brew --prefix zlib)/lib -L(brew --prefix sqlite)/lib -L(brew --prefix bzip2)/lib"
+    case Linux
+        if type -d /home/linuxbrew/.linuxbrew
+            set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
+            set -gx HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
+            set -gx HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
+            set -q PATH; or set PATH ''; set -gx PATH "/home/linuxbrew/.linuxbrew/bin" "/home/linuxbrew/.linuxbrew/sbin" $PATH;
+            set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/home/linuxbrew/.linuxbrew/share/man" $MANPATH;
+            set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/home/linuxbrew/.linuxbrew/share/info" $INFOPATH;
+        end
 end
 
 if type -q starship
