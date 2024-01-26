5752478769acad4ceafc1d700dcd94c4968a927f
Justin Bennett <git@just-be.dev>
2024-01-26 12:12:43 -0500

    Fix up nexus testing function

--- functions/nt.fish
+++ functions/nt.fish
@@ -1,5 +1,12 @@
 # Oxide
 function nt --description "Command to run tests for omicron"
-    echo (tint: black "cargo nextest run -E \"test($argv)\"")
-    cargo nextest run -p omicron-nexus -E "test($argv)"
+    argparse n/nocapture -- $argv
+    or return
+    if set -ql _flag_nocapture
+        echo (tint: black "cargo nextest run --no-capture -E \"test($argv)\"")
+        cargo nextest run --no-capture -p omicron-nexus -E "test($argv)"
+    else
+        echo (tint: black "cargo nextest run -E \"test($argv)\"")
+        cargo nextest run -p omicron-nexus -E "test($argv)"
+    end
 end
