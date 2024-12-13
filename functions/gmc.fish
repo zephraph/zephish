10243a04aecdf17ce9fb35c0a0b9ff03be2089eb
Justin Bennett <git@just-be.dev>
2024-12-12 23:46:03 -0500

    Update gmc to accept base branch

--- functions/gmc.fish
+++ functions/gmc.fish
@@ -1,40 +1,45 @@
 function gmc --description "Move git commit to another branch"
     # Check if branch name argument is provided
     if test (count $argv) -lt 1
-        echo "Usage: gmc <branch_name> [number_of_commits]"
+        echo "Usage: gmc <target_branch_name> [base_branch]"
         echo " "
-        echo (tint: grey "Moves git commits from one branch to another")
+        echo (tint: grey "Moves last git commit to the target branch, creating it if it doesn't exist")
         return 1
     end
 
-    # Store current branch name
+    # Store current branch name and target branch
     set current_branch (git branch --show-current)
-    set target_branch $argv[1]
-    set num_commits 1
+    set new_branch $argv[1]
+    set base_branch main
 
-    # If second argument is provided, use it as number of commits
+    # If second argument is provided, use it as base branch
     if test (count $argv) -ge 2
-        set num_commits $argv[2]
+        set base_branch $argv[2]
     end
 
-    # Check if target branch exists, if not create it from main/master
-    if not git show-ref --verify --quiet refs/heads/$target_branch
-        # Determine if main or master is used
-        if git show-ref --verify --quiet refs/heads/main
-            git checkout -b $target_branch main
-        else
-            git checkout -b $target_branch master
+    # Check if target branch exists
+    if git show-ref --verify --quiet refs/heads/$new_branch
+        if test (count $argv) -ge 2
+            echo "Error: Branch '$new_branch' already exists and base branch was specified"
+            return 1
         end
     else
-        # Switch to target branch if it exists
-        git checkout $target_branch
+        # Create new branch from base_branch
+        if not git show-ref --verify --quiet refs/heads/$base_branch
+            # If specified base branch doesn't exist, try master
+            if test $base_branch = main -a (git show-ref --verify --quiet refs/heads/master)
+                set base_branch master
+            else
+                echo "Error: Base branch '$base_branch' does not exist"
+                return 1
+            end
+        end
+        git checkout -b $new_branch $base_branch
     end
 
-    # Get the last N commits from original branch
-    set commits (git rev-parse $current_branch~$num_commits..$current_branch)
+    # Get the last commit from original branch
+    set commit (git rev-parse $current_branch~1..$current_branch)
 
-    # Cherry pick the commits in reverse order (oldest first)
-    for commit in $commits[-1..1]
-        git cherry-pick $commit
-    end
+    # Cherry pick the commit
+    git cherry-pick $commit
 end
